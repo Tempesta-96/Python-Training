9addf2d5d0c08c0995e803217f8bdca2605e3f82
valente <perfectnight8@gmail.com>
2019-07-24 21:10:23 +0800

    second

---
+++ testing.py
@@ -0,0 +1,18 @@
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = "Blue"
+my_hair = "Brown"
+
+
+print (f"He's {my_height} ")
+total =  my_age + my_height + my_weight
+
+print (f"my {total}")
+
+hiliarious = False 
+print ("isnt that joke funny {}".format(hiliarious))
+print (" {}".format('snow'))
+
+print ("jan \nFeb")
+print ("i am 6\'2 \" tall.")
